a3b20df491874be7364d152feb358b347d70bda2
rahulworkmail41-sudo <rahulworkmail41@gmail.com>
2026-04-22 14:50:09 -0400

    Add files via upload

---
+++ WEEK_3/Sentiment_analysis_bot/config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
+    APP_TENANTID = os.environ.get("MicrosoftAppTenantId", "")
+    # Added to support interaction with Azure AI Language API
+    ENDPOINT_URI = os.environ.get("MicrosoftAIServiceEndpoint","")
+    #print(f"ENDPOINT_URI = {ENDPOINT_URI}")
+    API_KEY = os.environ.get("MicrosoftAPIKey","")
